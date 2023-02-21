8b7aaffb4dfa878f18092159ec399446d97e4bc4
Olha Lushina <olga.lushina.ua@gmail.com>
2023-02-21 12:39:45 +0200

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+    
+const galleryEl = document.querySelector('.gallery');
+const galleryItemsEl = createGalleryItemsEl(galleryItems);
+let modale;
 
-console.log(galleryItems);
+// Додаємо елементи до галереї 
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsEl);
+
+// Прослуховуємо подію кліка на елементах галереї
+galleryEl.addEventListener('click', onImageClick);
+
+// Прослуховуємо подію натиснення клавіші Escape
+document.addEventListener('keydown', onDocumentKeydown);
+
+// Створення розмітки галереї
+function createGalleryItemsEl(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    }).join('');
+}
+
+// Обробник події кліка на зображенню галереї
+function onImageClick(e) {
+    // Забороняємо перехід за посиланням за замовчуванням
+    e.preventDefault(); 
+
+    // Створюємо модальне вікно
+    modale = basicLightbox.create(`
+        <img src="${e.target.parentNode.href}" width="800" height="600">
+    `);
+
+    // Відкриваємо модальне вікно
+    modale.show();
+}
+
+// Обробник натиснення клавіші Escape
+function onDocumentKeydown(e) {
+    if (e.code !== 'Escape') {
+        return;
+    }
+
+    // Закриваємо модальне вікно
+    modale.close();
+}
